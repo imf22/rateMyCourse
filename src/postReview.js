ef527575b38fef242a0913badd51fc47e1124aed
Medha Gupta <medhagupta13@gmail.com>
2022-02-22 13:36:53 -0500

    postReview HTML and JS pages

---
+++ src/postReview.js
@@ -0,0 +1,13 @@
+
+
+
+let name = document.getElementById("name");
+
+let foodTruck  = document.getElementById("ft-select");
+
+let rating = document.getElementById("rating");
+
+let review = document.getElementById("review");
+
+let sendButton = document.getElementById("get-button");
+
