4deb7f3a342530ad600ca29fc9dbee0c6cc95337
WPF27 <77469095+WPF27@users.noreply.github.com>
2022-02-26 14:45:50 -0500

    Upload DB and sample usage

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,18 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('data.db');
+//db.exec('INSERT INTO TRUCKS (TRUCK_ID,USER_NAME,TRUCK_NAME,RATING,FOOD_TYPE,REVIEW) VALUES (1,"TEST","TEST",5,"TEST","GOOD")');
+let sql = 'SELECT * FROM TRUCKS';
+db.all(sql,  (err, row) => {
+    if (err) {
+        return console.error(err.message)
+    }
+    return console.log(row)
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
